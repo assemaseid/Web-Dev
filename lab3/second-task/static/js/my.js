09978f5b9dbec4c70ddfd6b355d5fbcdc5b6e6d4
assemaseid <seidkarim601705@gmail.com>
2025-01-31 00:17:13 +0500

    second task finished

---
+++ lab3/second-task/static/js/my.js
@@ -0,0 +1,45 @@
+let display = document.getElementById("display");
+let btn = document.getElementById("btn");
+let todos = document.getElementById("todos");
+
+btn.addEventListener("click", function () {
+    let new_task = display.value;
+    if (new_task) {
+        let taskDiv = document.createElement('div');
+        taskDiv.classList.add("task-item");
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("task-checkbox");
+
+        let taskText = document.createElement('span');
+        taskText.textContent = new_task;
+        taskText.classList.add("task-text");
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerHTML = "🗑";
+        deleteBtn.classList.add("delete-btn");
+
+        checkbox.addEventListener("change", function () {
+            if (checkbox.checked) {
+                taskText.classList.add("completed");
+                taskText.classList.toggle("deleted");
+            } else {
+                taskText.classList.remove("completed");
+            }
+        });
+
+        deleteBtn.addEventListener("click", function () {
+            taskDiv.remove();
+        });
+
+        taskDiv.appendChild(checkbox);
+        taskDiv.appendChild(taskText);
+        taskDiv.appendChild(deleteBtn);
+        todos.appendChild(taskDiv);
+
+        display.value = '';
+    } else {
+        alert("Please, enter the task description!");
+    }
+});
